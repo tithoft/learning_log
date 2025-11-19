206ed6d730523f672275c69947bee7b24778f09a
tithoft <99927615+tithoft@users.noreply.github.com>
2025-11-19 15:10:46 -0500

    Added bootstrap styling to login, topics, entries,
    and index.

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party apps.
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
